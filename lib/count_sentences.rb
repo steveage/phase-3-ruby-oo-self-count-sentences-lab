9e00b0c88c42262b262d4c9a07a876ab2394996a
Mariusz <mariuszbia@yahoo.com>
2022-07-31 11:31:04 -0500

    Completed assignment.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    ends_with?(".") 
   end
 
   def question?
-
+    ends_with?("?")
   end
 
   def exclamation?
+    ends_with?("!")
+  end
 
+  def ends_with?(character)
+    self.end_with?(character)
   end
 
   def count_sentences
-
+    pattern = "[.!?](?=[\w ])"
+    regexp = Regexp.new(pattern)
+    self.split(regexp).length()
   end
 end
